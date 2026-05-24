6d30b55b203821a849326151de223f0e3c11afd8
Kristofer <kristofer.cepon.povsic@gmail.com>
2026-05-24 14:36:01 +0200

    Sem. presentation: footline config

--- 2semester/seminar/presentation/.auctex-auto/presentation.el
+++ 2semester/seminar/presentation/.auctex-auto/presentation.el
@@ -6,7 +6,15 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "14pt" "t")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("fontenc" "T1") ("graphicx" "") ("amsmath" "") ("amssymb" "") ("gensymb" "") ("babel" "slovene") ("siunitx" "") ("pgfplots" "")))
+                     '(("pgfplots" "") ("fontenc" "T1") ("graphicx" "") ("amsmath" "") ("amssymb" "") ("gensymb" "") ("babel" "slovene") ("biblatex" "sorting=none" "style=numeric" "sortcites=true") ("siunitx" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -17,7 +25,9 @@
     "amssymb"
     "gensymb"
     "babel"
-    "siunitx"
-    "pgfplots"))
+    "biblatex"
+    "siunitx")
+   (LaTeX-add-bibliographies
+    "../source/refs"))
  :latex)
 
