23a0ce9f3f2abdd07ec3ca848fcb3785d48e64d7
Kristofer <kristofer.cepon.povsic@gmail.com>
2026-05-26 10:43:38 +0200

    Sem. presentation: corrections

--- 2semester/seminar/presentation/.auctex-auto/presentation.el
+++ 2semester/seminar/presentation/.auctex-auto/presentation.el
@@ -8,12 +8,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("pgfplots" "") ("fontenc" "T1") ("graphicx" "") ("amsmath" "") ("amssymb" "") ("gensymb" "") ("babel" "slovene") ("biblatex" "sorting=none" "style=numeric" "sortcites=true") ("siunitx" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
