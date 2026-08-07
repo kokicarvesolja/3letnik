190b375c27a5da2bbda059cefedec9f3cc93dc7b
Kristofer <kristofer.cepon.povsic@gmail.com>
2026-08-07 21:57:22 +0200

    MAF2: Additional orthogonality

--- 1semester/mafija2/06zakljucna/source/main.py
+++ 1semester/mafija2/06zakljucna/source/main.py
@@ -44,23 +44,11 @@ omega = (1 / 2) * np.sqrt(g / L) * xi[:, 0]
 n = [2, 5, 6, 7]
 x_kord = np.linspace(0, 1, m)
 
-# druga ničla
+for i in n:
+    value1 = np.trapezoid(ss.j0(xi[2, 0] * np.sqrt(1 - x_kord / L)) * ss.j0(xi[i, 0] * np.sqrt(1 - x_kord / L)), x_kord)
+    print(f'{i} ', value1)
 
-xi_2 = ss.jn_zeros(0, 2)[-1]
 
-# tretja ničla
-
-xi_3 = ss.jn_zeros(0, 3)[-1]
-
-value2 = si.quad(lambda x: ss.j0(xi_2 * np.sqrt(1 - x / L)) * ss.j0(xi_2 * np.sqrt(1 - x / L)), 0, 1)
-value3 = si.quad(lambda x: ss.j0(xi_2 * np.sqrt(1 - x / L)) * ss.j0(xi_3 * np.sqrt(1 - x / L)), 0, 1)
-
-print("scipy.integrate ", value2, ' ', value3)
-
-valueNP2 = np.trapezoid(ss.j0(xi_2 * np.sqrt(1 - x_kord/ L)) * ss.j0(xi_2 * np.sqrt(1 - x_kord / L)), x_kord)
-valueNP3 = np.trapezoid(ss.j0(xi_2 * np.sqrt(1 - x_kord/ L)) * ss.j0(xi_3 * np.sqrt(1 - x_kord / L)), x_kord)
-
-print("numpy ", valueNP2, ' ', valueNP3)
 
 '''
 # gradnja intervala velikosti (N, m)
