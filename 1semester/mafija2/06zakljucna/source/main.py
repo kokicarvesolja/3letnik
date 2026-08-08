2985992737e8f6d8c3414b445f3eaa028c6766ff
Kristofer <kristofer.cepon.povsic@gmail.com>
2026-08-08 19:30:14 +0200

    MAF2: intermezzo

--- 1semester/mafija2/06zakljucna/source/main.py
+++ 1semester/mafija2/06zakljucna/source/main.py
@@ -11,21 +11,27 @@ g = 9.81
 L = 1.0
 v0 = 1.0
 sigma = 0.05 * L
-m = 10000
+m = 1000
 N = 100
 
 def stevec(x, nicle):
     '''
     Funkcija kot parameter vzame x, array dimenzije (N, m), in nicle, array dimenzije (N, m).
     '''
-    return v0 * np.exp(- (x - L) ** 2 / (2 * sigma ** 2)) * ss.j0(nicle * np.sqrt(1 - x / L))
+    return v0 * np.exp(- (x - L) ** 2 / (2 * sigma ** 2)) * x * ss.j0(nicle * np.sqrt(1 - x / L))
+
+def alt_stevec(x, nicle):
+    '''
+    f(x) = 1
+    '''
+    return x * ss.j0(nicle * x)
 
 def imenovalec(x, nicle):
     '''
     Funkcija kot parametra sprejme x, array dimenzije m, in nicle, array dimenzije N, ki je N ničel Besslove funkcije J_0.
     Vrne array dimenzije (N, m).
     '''
-    return (ss.j0(nicle * np.sqrt(1 - x / L))) ** 2
+    return (ss.j0(nicle * np.sqrt(1 - x / L))) ** 2 * x
 
 def gauss(v0, sigma, x):
     return v0 * np.exp(- (x - L) ** 2 / (2 * sigma ** 2))
@@ -56,17 +62,62 @@ def cumulative_tail(n, koef):
 # ničle Besselovih funkcij, ki jih shranimo v array dimenzij (N, m)
 # (N,) preko broadcast_to v (m, N) in s .T v (N, m)
 
-xi = np.broadcast_to(ss.jn_zeros(0, N), (m + 100, N)).T
+xi = np.broadcast_to(ss.jn_zeros(0, N), (m, N)).T
 
 # frekvenca omega_n v arrayju dimenzije (N,)
 
 omega = (1 / 2) * np.sqrt(g / L) * xi[:, 0]
 
+# --------------- intermezzo ---------------
+
+# za funkcijo f(x) vem, da imajo koeficienti vrednost
+# B_n = 2/(xi_n * J_1 (xi_n))
+# source: https://math.libretexts.org/Bookshelves/Differential_Equations/Introduction_to_Partial_Differential_Equations_(Herman)/05%3A_Non-sinusoidal_Harmonics_and_Special_Functions/5.05%3A_Fourier-Bessel_Series
+
+# interval za integriranje
+
+uniform = np.linspace(0, 1, m)
+
+# izračun koeficientov preko integracije
+
+interval = np.broadcast_to(uniform, (N, m))
+
+# integracija
+
+koef_int = np.trapezoid(alt_stevec(interval, xi), uniform, axis=1)
+
+koef_int = (2 / ((ss.j1(xi[:, 0]) ** 2)) * koef_int) 
+print('koef_int ', koef_int)
+
+# izračun koeficientov preko identitete
+
+koef_id = (2 / (xi[:, 0] * ss.j1(xi[:, 0])))
+print('koef_id ', koef_id)
+
+# prvih 50 členov
+temp_int = 0
+temp_id = 0
+for i in range(N):
+    temp_int += koef_int[i] * ss.j0(xi[i, 0] * uniform)
+    temp_id += koef_id[i] * ss.j0(xi[i, 0] * uniform)
+
+plt.plot(uniform, temp_int, label='int', alpha=0.5)
+plt.plot(uniform, temp_id, label='id', alpha=0.5)
+plt.legend()
+plt.show()
+
+plt.plot(uniform, np.abs(temp_int - temp_id))
+plt.show()
+
+
+# --------------- konec intermezza ---------------
+
 # gradnja intervala velikosti (N, m)
 
 non_uniform = np.r_[np.linspace(0, 0.5, 100, endpoint=False), np.linspace(0.5, 1, m)]
+uniform = np.linspace(0, 1, m)
 
-interval = np.broadcast_to(non_uniform, (N, m+100))
+interval = np.broadcast_to(uniform, (N, m))
 
 # gradnja matrike z rezultati, dimenzij (N, 3)
 
@@ -77,51 +128,21 @@ y = np.arange(N, step=1.0)
 _, rez = np.meshgrid(x, y) 
 
 # stevec
-rez[:, 0] = np.trapezoid(stevec(interval, xi), non_uniform, axis=1)
+rez[:, 0] = np.trapezoid(alt_stevec(interval, xi), uniform, axis=1)
 # imenovalec
-rez[:, 1] = np.trapezoid(imenovalec(interval, xi), non_uniform, axis=1)
+rez[:, 1] = ss.j1(xi[:, 0]) ** 2
 # koeficienti
-rez[:, 2] = (1 / omega) * (rez[:, 0] / rez[:, 1]) 
+rez[:, 2] = (2 / omega) * (rez[:, 0] / rez[:, 1]) 
 
 # initial condition plot
 
-x_kord = np.r_[np.linspace(0, 0.5, 100, endpoint=False), np.linspace(0.5, 1, m)]
+x_kord = uniform
 
 j0i = 0 # rez[N-1, 2] * ss.j0(xi[N-1, 0] * np.sqrt(1 - x_kord / L))
 
 for i in range(N):
-    j0i += rez[i, 2] * ss.j0(xi[i, 0] * np.sqrt(1 - x_kord / L))
+    j0i += rez[i, 2] * ss.j0(xi[i, 0] * uniform)
 
-plt.plot(x_kord, j0i)
+#plt.plot(x_kord, j0i)
 #plt.plot(x_kord, gauss(v0, sigma, x_kord))
-
-plt.show()
-
-
-
-'''
-
-for i in range(N):
-    num = np.trapezoid(stevec(interval, i + 1), interval)
-    den = np.trapezoid(imenovalec(interval, i + 1), interval)
-    b_n = (1 / omega(i + 1)) * (num / den)
-    rez[i] = b_n 
-
-#plt.plot(np.arange(N, step=1), np.cumsum(rez))
-
-plt.stem(np.arange(N, step=1), np.abs(rez), markerfmt='.')
-
-plt.show()
-
-plt.plot(np.arange(N, step=1), np.cumsum(np.abs(rez)))
-
-plt.show()
-'''
-    
-
-    
-    
-
-
-
 
