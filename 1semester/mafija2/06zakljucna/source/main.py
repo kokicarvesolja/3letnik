0d97c35bd97c8409a1e4728084c7300b8405bd45
Kristofer <kristofer.cepon.povsic@gmail.com>
2026-08-08 18:12:21 +0200

    MAF2: Deceived by AI

--- 1semester/mafija2/06zakljucna/source/main.py
+++ 1semester/mafija2/06zakljucna/source/main.py
@@ -11,8 +11,8 @@ g = 9.81
 L = 1.0
 v0 = 1.0
 sigma = 0.05 * L
-m = 1000
-N = 20000
+m = 10000
+N = 100
 
 def stevec(x, nicle):
     '''
@@ -30,10 +30,33 @@ def imenovalec(x, nicle):
 def gauss(v0, sigma, x):
     return v0 * np.exp(- (x - L) ** 2 / (2 * sigma ** 2))
 
+def stem_plot(n, koef):
+    '''
+    Makes a stem plot of |B_n| in relation to n
+    '''
+    x = np.arange(n, step=1)
+    plt.stem(x, np.abs(koef), markerfmt='.')
+    plt.xlabel(r'$n$')
+    plt.ylabel(r'$\left| B_N \right|$')
+    plt.show()
+    pass
+
+def cumulative_tail(n, koef):
+    '''
+    Makes a cumulative tail plot
+    '''
+    x = np.arange(n, step=1)
+    plt.plot(x, np.cumsum(np.abs(koef)))
+    plt.xlabel(r'$n$')
+    plt.ylabel(r'$\sum_{n = 1}^{N}\left| B_N \right|^2$')
+    plt.show()
+    pass
+
+
 # ničle Besselovih funkcij, ki jih shranimo v array dimenzij (N, m)
 # (N,) preko broadcast_to v (m, N) in s .T v (N, m)
 
-xi = np.broadcast_to(ss.jn_zeros(0, N), (m, N)).T
+xi = np.broadcast_to(ss.jn_zeros(0, N), (m + 100, N)).T
 
 # frekvenca omega_n v arrayju dimenzije (N,)
 
@@ -41,7 +64,9 @@ omega = (1 / 2) * np.sqrt(g / L) * xi[:, 0]
 
 # gradnja intervala velikosti (N, m)
 
-interval = np.broadcast_to(np.linspace(0, 1, m), (N, m))
+non_uniform = np.r_[np.linspace(0, 0.5, 100, endpoint=False), np.linspace(0.5, 1, m)]
+
+interval = np.broadcast_to(non_uniform, (N, m+100))
 
 # gradnja matrike z rezultati, dimenzij (N, 3)
 
@@ -52,24 +77,29 @@ y = np.arange(N, step=1.0)
 _, rez = np.meshgrid(x, y) 
 
 # stevec
-rez[:, 0] = np.trapezoid(stevec(interval, xi), np.linspace(0, 1, m), axis=1)
+rez[:, 0] = np.trapezoid(stevec(interval, xi), non_uniform, axis=1)
 # imenovalec
-rez[:, 1] = np.trapezoid(imenovalec(interval, xi), np.linspace(0, 1, m), axis=1)
+rez[:, 1] = np.trapezoid(imenovalec(interval, xi), non_uniform, axis=1)
 # koeficienti
 rez[:, 2] = (1 / omega) * (rez[:, 0] / rez[:, 1]) 
 
-#plt.stem(np.arange(N, step=1), np.log(np.abs(rez[:, 2])), markerfmt='.')
-plt.plot(np.arange(N, step=1), np.cumsum(np.abs(rez[:, 2])))
-#plt.semilogy(np.arange(N, step=1), np.cumsum(np.abs(rez[:, 2])))
+# initial condition plot
 
-plt.xlabel('n')
-plt.ylabel(r'$\sum_{n = 1}^{N}\left| B_n \right|^2$')
+x_kord = np.r_[np.linspace(0, 0.5, 100, endpoint=False), np.linspace(0.5, 1, m)]
+
+j0i = 0 # rez[N-1, 2] * ss.j0(xi[N-1, 0] * np.sqrt(1 - x_kord / L))
+
+for i in range(N):
+    j0i += rez[i, 2] * ss.j0(xi[i, 0] * np.sqrt(1 - x_kord / L))
+
+plt.plot(x_kord, j0i)
+#plt.plot(x_kord, gauss(v0, sigma, x_kord))
+
 plt.show()
 
 
 
 '''
-# gradnja matrike Y dimenzij (N, M)
 
 for i in range(N):
     num = np.trapezoid(stevec(interval, i + 1), interval)
