ab67b75804a22065ba2b52ca072f7e2e26350483
Kristofer <kristofer.cepon.povsic@gmail.com>
2026-08-10 18:10:25 +0200

    MAF2: Convergence plots finished

--- 1semester/mafija2/06zakljucna/source/main.py
+++ 1semester/mafija2/06zakljucna/source/main.py
@@ -4,20 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as ss
 import scipy.integrate as si
+import cmasher as cmr
+
 
 # Constants
 
 g = 9.81
 L = 1.0
 v0 = 1.0
-m = 1000
-N = 100
+M = 1000
+N = 180
 
-def stevec(x, nicle):
+def nicle(n, m):
+    '''
+    For easier integration, returns array of size (n, m) with the n-th zero of J0 along the axis =1 
+    '''
+    # ničle Besselovih funkcij, ki jih shranimo v array dimenzij (N, m)
+    # (N,) preko broadcast_to v (m, N) in s .T v (N, m)
+    xi = np.broadcast_to(ss.jn_zeros(0, n), (m, n)).T
+    return xi
+    
+
+def stevec(x, n, m, zero=nicle):
     '''
     Funkcija kot parameter vzame x, array dimenzije (N, m), in nicle, array dimenzije (N, m).
     '''
-    return ss.j0(nicle * np.sqrt(1 - x / L))
+    return ss.j0(zero(n,m) * np.sqrt(1 - x / L))
 
 def alt_stevec(x, nicle):
     '''
@@ -36,7 +48,7 @@ def stem_plot(n, koef):
     plt.show()
     pass
 
-def cumulative_tail(n, koef):
+def cumulative_tail(n, m, koef):
     '''
     Makes a cumulative tail plot
     '''
@@ -47,60 +59,92 @@ def cumulative_tail(n, koef):
     plt.show()
     pass
 
-def u_n(n, koef):
+def u_n(n, m, koef, zero=nicle):
     '''
     Returns the sum of n-terms in the Fourier Bessel series
     '''
-    temp = 0
+    uniform = np.linspace(0.95 * L, 1 * L, m)
+    # frekvence
+    omega = (1 / 2) * np.sqrt(g / L) * zero(n, m)[:, 0]
+    # vsota
+    sumJ = 0
     for i in range(n):
-        temp += koef[i] * ss.j0(xi[i, 0] * np.sqrt(1 - uniform/L))
-    return temp
-        
+        sumJ += koef(n, m)[i] * ss.j0(zero(n, m)[i, 0] * np.sqrt(1 - uniform/L))
+    return sumJ
 
-# ničle Besselovih funkcij, ki jih shranimo v array dimenzij (N, m)
-# (N,) preko broadcast_to v (m, N) in s .T v (N, m)
+def koef_n(n, m, zero=nicle, kick=0.95, L=1, g=9.81):
+    '''
+    Returns array of length n with coefficients of Fourier-Bessel series.
+    '''
+    # frekvenca omega_n v arrayju dimenzije (n,)
+    omega = (1 / 2) * np.sqrt(g / L) * zero(n, m)[:, 0]
 
-xi = np.broadcast_to(ss.jn_zeros(0, N), (m, N)).T
+    # gradnja intervala velikosti (N, m)
+    uniform = np.linspace(0.95 * L, 1 * L, m)
+    interval = np.broadcast_to(uniform, (n, m))
 
-# frekvenca omega_n v arrayju dimenzije (N,)
+    # gradnja matrike z rezultati, dimenzij (N, 3)
+    x = np.arange(3, step=1.0)
+    y = np.arange(n, step=1.0)
 
-omega = (1 / 2) * np.sqrt(g / L) * xi[:, 0]
+    # matrika oblike [[0, 0, 0], [1, 1, 1], ..., [N, N, N]]
+    _, rez = np.meshgrid(x, y)
+    # stevec
+    rez[:, 0] = np.trapezoid(stevec(interval, n, m, zero), uniform, axis=1)
+    # imenovalec
+    rez[:, 1] = ss.j1(zero(n, m)[:, 0]) ** 2
+    # koeficienti
+    rez[:, 2] = (rez[:, 0] / rez[:, 1]) 
+    return rez[:, 2]
 
-# gradnja intervala velikosti (N, m)
+# convergence in L2 space
 
-uniform = np.linspace(0.95, 1, m)
+mja = [1000, 10000]
+sums = [i for i in range(25, 275, 25)]
 
-interval = np.broadcast_to(uniform, (N, m))
+# colors
 
-# gradnja matrike z rezultati, dimenzij (N, 3)
+colors = cmr.take_cmap_colors("cmr.cosmic", len(sums), cmap_range=(0.2, 1),
+                              return_fmt="hex")
 
-x = np.arange(3, step=1.0)
-y = np.arange(N, step=1.0)
 
-# matrika oblike [[0, 0, 0], [1, 1, 1], ..., [N, N, N]]
-_, rez = np.meshgrid(x, y) 
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8))
 
-# stevec
-rez[:, 0] = np.trapezoid(stevec(interval, xi), uniform, axis=1)
-# imenovalec
-rez[:, 1] = ss.j1(xi[:, 0]) ** 2
-# koeficienti
-rez[:, 2] = (rez[:, 0] / rez[:, 1]) 
+'''
+for u, ax in zip(mja, [ax1, ax2]):
+    for s, c in zip(sums, colors):
+        print("Racunam za ", s)
+        uniform = np.linspace(0.95, 1, u)
+        error = np.sqrt(np.trapezoid((1 - u_n(s, u, koef=koef_n)) ** 2, uniform))
+        ax.scatter(s, np.max((error)), marker='.', color=c)
 
-stem_plot(N, rez[:, 2])
+ax1.set_title(r'M = 1000')
+ax2.set_title(r'M = 10000')
+fig.supxlabel(r'$n$')
+fig.supylabel(r'$I_{L_2}$')
+fig.suptitle(r'Konvergenca v $L_2$ normi')
+fig.tight_layout
+fig.savefig('konvergencaL2.png')
+fig.show()
 
-# initial condition plot
+'''
 
-j0i = 0 # rez[N-1, 2] * ss.j0(xi[N-1, 0] * np.sqrt(1 - x_kord / L))
+'''
+# pointwise convergence
 
-for i in range(N):
-    j0i += rez[i, 2] * ss.j0(xi[i, 0] * np.sqrt(1 - uniform/L))
-
-x_kord = np.r_[np.linspace(0, 0.95, m), uniform]
-j0i = np.r_[np.zeros(m), j0i]
-
-plt.plot(x_kord, j0i)
-plt.ylim(0, 2)
+for u, ax in zip(mja, [ax1, ax2]):
+    for s, c in zip(sums, colors):
+        print("Racunam za ", s)
+        error = np.max(np.abs(1 - u_n(s, u, koef_n)[200:]))
+        ax.scatter(s, error, marker='.', color=c)
+    
+ax1.set_title(r'M = 1000')
+ax2.set_title(r'M = 10000')
+fig.supxlabel(r'$n$')
+fig.supylabel(r'$I_{\text{tocke}}$')
+fig.suptitle(r'Konvergenca po točkah')
+fig.tight_layout
+fig.savefig('konvergencaTocke.png')
 plt.show()
-#plt.plot(x_kord, gauss(v0, sigma, x_kord))
 
+'''
